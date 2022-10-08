e53527776aa98938501b4ef043f731ddfe62f9f3
Trevor Brooks <trevorism@gmail.com>
2022-10-08 18:44:21 -0400

    Need to get metaclass on the class not the instance

--- src/test/groovy/com/trevorism/gcloud/webapi/service/DatastoreFilterServiceTest.groovy
+++ src/test/groovy/com/trevorism/gcloud/webapi/service/DatastoreFilterServiceTest.groovy
@@ -93,7 +93,7 @@ class DatastoreFilterServiceTest {
     @Test
     void testCreateIdFilter() {
         DatastoreFilterService service = new DatastoreFilterService()
-        service.metaClass.getDatastore = {[newKeyFactory: { new KeyFactory("trevorism-gcloud")}] as Datastore}
+        DatastoreFilterService.metaClass.getDatastore = {[newKeyFactory: { new KeyFactory("trevorism-gcloud")}] as Datastore}
         def simpleFilter = new SimpleFilter(type: FilterConstants.TYPE_NUMBER, field: "id", operator: FilterConstants.OPERATOR_EQUAL, value: "59234923")
         StructuredQuery.Filter filter = service.createSimpleFilter(simpleFilter, "kind")
 
